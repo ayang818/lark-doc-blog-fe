b64f634d4a74e21017645ea653ed10ca9e31e077
Yangfengchang <ayang818@qq.com>
2021-12-28 15:16:03 +0800

    fix bug

--- src/component/BlogNav/index.jsx
+++ src/component/BlogNav/index.jsx
@@ -19,14 +19,22 @@ export default class NavApp extends React.Component {
                     <Col xs={3} sm={7} md={7} lg={7} xl={7} xxl={7} className='h100'>
                     </Col>
                     <Col xs={18} sm={10} md={10} lg={10} xl={10} xxl={10} className='flex h100' style={{padding: '0 0 2px 0'}}>
-                        <Link to='' className='miss-ul udl'><Title heading={4}>Frazier Yang</Title></Link>
-                        <div className='flex h100' style={{width: '20%'}}>
-                            <Link to='blog' className='miss-ul udl' style={{padding: '0 0 2px 0'}}><Text strong={true}>Blog</Text></Link>
-                            <a href='https://www.github.com/ayang818' className='udl' style={{padding: '3px 0 0 0'}} target='blank'><IconGithubLogo style={{color: 'rgba(var(--semi-grey-8), 1)'}}/></a>
+                        <Link to='' className='miss-ul udl'>
+                            <Title heading={4}>Frazier Yang</Title>
+                        </Link>
+
+                        <div className='flex h100' style={{width: '25%'}}>
+                            <Link to='blog' className='miss-ul udl' style={{padding: '0 0 2px 0'}}>
+                                <Text strong={true}>Blog</Text>
+                            </Link>
+                            <a href='https://www.github.com/ayang818' className='udl' style={{padding: '3px 0 0 0'}} target='blank'>
+                                <IconGithubLogo style={{color: 'rgba(var(--semi-grey-8), 1)'}}/>
+                            </a>
                             {/* 开关 */}
-                            <div onClick={this.click} className='udl' style={{padding: '3px 0 0 0'}}>{
-                            this.props.isLight ? <IconSun style={{color: 'rgba(var(--semi-orange-2), 1)'}}/> : <IconMoon style={{color: 'rgba(var(--semi-light-blue-8), 1)'}}/>
-                            }
+                            <div onClick={this.click} className='udl' style={{padding: '3px 0 0 0'}}>
+                                {
+                                    this.props.isLight ? <IconSun style={{color: 'rgba(var(--semi-orange-2), 1)'}}/> : <IconMoon style={{color: 'rgba(var(--semi-light-blue-8), 1)'}}/>
+                                }
                             </div>
                         </div>
                     </Col>
